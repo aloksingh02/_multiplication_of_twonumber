bac4d30a79c6b0f183363cf910aea4e4c504e25f
Alok KumarS Singh <singhalokkumar427@gmail.com>
2022-02-15 00:07:07 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a=12;
+        int b=89;
+        int z=a*b;
+        System.out.println(z);
+	// write your code here
+    }
+}
